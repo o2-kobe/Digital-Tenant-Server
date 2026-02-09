8f8aa42b6a37c66415ea2d4a2577cc10cbfb0fc4
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-09 16:24:50 +0000

    feat: add property model linked to landlord

---
+++ src/model/property.model.ts
@@ -0,0 +1,47 @@
+import { Document, model, Schema, Types } from "mongoose";
+
+export interface PropertyDocument extends Document {
+  landlordId: Types.ObjectId;
+  name: string;
+  city: string;
+  town: string;
+  image: string;
+  address: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const propertySchema = new Schema<PropertyDocument>(
+  {
+    landlordId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: { type: String, required: true, min: 3 },
+    city: { type: String, required: true, min: 3 },
+    town: { type: String, required: true, min: 3 },
+    image: { type: String, required: true, min: 3 },
+    address: { type: String, required: true, min: 3 }, //make this optional
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Create index for document
+propertySchema.index({ landlordId: 1, createdAt: -1 });
+
+propertySchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform(_doc, ret: any) {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+const Property = model<PropertyDocument>("Property", propertySchema);
+
+export default Property;
