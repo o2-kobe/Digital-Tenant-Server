c4f32916d8dd9420487ac7dd6c58a3c517d38850
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-10 21:36:06 +0000

    chore: restrict payload size for secure system

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { generateRandomKey } from "./utils/helper";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 console.log(generateRandomKey(6));
 app.listen(400, () => console.log("App started successfully"));
