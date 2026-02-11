fe47f6df8084bbc57257797cd205b66a39c7823d
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-11 15:39:52 +0000

    feat: add proper schemas for validation on request made

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,28 +1,28 @@
-import { object, z } from "zod";
+import { z } from "zod";
 
-export const createUserSchema = object({
-  body: object({
-    username: z
-      .string()
-      .nonempty("Username is required")
-      .min(4, "Userame must be at least 4 characters"),
+// Body schema
+const userBody = z
+  .object({
+    username: z.string().min(4, "Username must be at least 4 characters"),
     role: z.enum(["tenant", "landlord"]),
     email: z.email(),
     password: z
       .string()
-      .nonempty("Password is required")
       .min(8, "Password must be at least 8 characters")
       .max(64, "Password cannot exceed 64 characters"),
-    passwordConfirm: z.string().nonempty("Please confirm password"),
-  }).refine((data) => data.password === data.passwordConfirm, {
+    passwordConfirm: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
     message: "Passwords do not match",
     path: ["passwordConfirm"],
-  }),
+  });
+
+// Route schemas
+export const createUserSchema = z.object({
+  body: userBody,
 });
 
-export type createUserRequest = z.infer<typeof createUserSchema>["body"];
+// Types
+export type CreateUserRequest = z.infer<typeof userBody>;
 
-export type CreateUserInput = Omit<
-  z.infer<typeof createUserSchema>["body"],
-  "passwordConfirm"
->;
+export type CreateUserInput = Omit<z.infer<typeof userBody>, "passwordConfirm">;
