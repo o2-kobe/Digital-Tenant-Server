457c681d276a5bb7fbe95b8b67654e14bacf2a91
Laud Tetteh <kobbykurtx@gmail.com>
2026-03-06 20:12:54 +0000

    refactor: update jwt utility function to use another algorithm and approach

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,19 +1,37 @@
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions, JwtPayload } from "jsonwebtoken";
 import config from "config";
 
-const privateKey = config.get<string>("privateKey");
-const publicKey = config.get<string>("publicKey");
+const JWT_SECRET = config.get<string>("JWT_SECRET");
+const issuer = config.get<string>("issuer");
+const audience = config.get<string>("audience");
 
-export function signJwt(object: Object, options?: jwt.SignOptions) {
-  return jwt.sign(object, privateKey, {
-    ...(options && options),
-    algorithm: "RS256",
+export interface AccessTokenPayload extends JwtPayload {
+  sub: string;
+  role: string;
+  session: string;
+}
+
+export function signJwt(payload: object, options?: SignOptions): string {
+  return jwt.sign(payload, JWT_SECRET, {
+    algorithm: "HS256",
+    issuer,
+    audience,
+    ...options,
   });
 }
 
-export function verifyJwt(token: string) {
+export function verifyJwt(token: string): {
+  decoded: AccessTokenPayload | null;
+  valid: boolean;
+  expired: boolean;
+} {
   try {
-    const decoded = jwt.verify(token, publicKey);
+    const decoded = jwt.verify(token, JWT_SECRET, {
+      algorithms: ["HS256"],
+      issuer,
+      audience,
+    }) as AccessTokenPayload;
+
     return {
       decoded,
       valid: true,
@@ -22,8 +40,8 @@ export function verifyJwt(token: string) {
   } catch (error: any) {
     return {
       decoded: null,
-      expired: (error.message = "JWT expired"),
       valid: false,
+      expired: error.name === "TokenExpiredError",
     };
   }
 }
