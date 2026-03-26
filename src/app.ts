4193e2df53e9b71f40e125a6e12273d882027e23
Laud Tetteh <kobbykurtx@gmail.com>
2026-03-26 20:37:39 +0000

    feat: add modules to connect and implement cookie parser to allow cookie to be sent

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,31 @@
 import express from "express";
-import { generateRandomKey } from "./utils/helper";
+import cookieParser from "cookie-parser";
+import routes from "./routes";
+import cors from "cors";
+import config from "config";
+import logger from "./utils/logger";
+import { connect } from "./utils/connect";
 
+const PORT = config.get<number>("port");
 const app = express();
-app.use(express.json({ limit: "10kb" }));
 
-console.log(generateRandomKey(6));
-app.listen(400, () => console.log("App started successfully"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+// All routes mounted here
+app.use("/api", routes);
+
+app.listen(PORT, async () => {
+  logger.info(`App connected successfully at ${PORT}...`);
+
+  await connect();
+});
+
+export default app;
